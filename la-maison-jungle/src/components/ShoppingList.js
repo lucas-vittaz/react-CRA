c83d7ac01d070c564c25783efba4de93c381dc3c
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-08-02 15:26:04 +0200

    destructuring

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -2,7 +2,7 @@ import { plantList } from '../datas/plantList'
 import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
 
-function ShoppingList() {
+function ShoppingList({cart, updateCart}) {
 	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
@@ -27,6 +27,7 @@ function ShoppingList() {
 					/>
 				))}
 			</ul>
+      <button onClick="">Ajouter au panier</button> 
 		</div>
 	)
 }
