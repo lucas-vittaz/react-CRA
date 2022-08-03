4fecad89013f90a516f4c22440609f9e0f43f7cf
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-08-03 14:58:15 +0200

    useState for cart

--- la-maison-jungle/src/components/PlantItem.js
+++ la-maison-jungle/src/components/PlantItem.js
@@ -5,9 +5,10 @@ function handleClick(plantName) {
 	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
 }
 
-function PlantItem({ cover, name, water, light }) {
+function PlantItem({ cover, name, water, light, price }) {
 	return (
 		<li className='plant-item' onClick={() => handleClick}>
+			<span className='plant-item-price'>{price}€</span>
 			<img className='plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
