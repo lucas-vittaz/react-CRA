91c9a46fdc4ef60bd5b6c721e212b92571d53a90
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-08-03 15:07:52 +0200

    refactoring

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,15 +1,13 @@
+import { useState } from 'react'
 import Banner from './Banner'
 import logo from '../assets/logo.png'
 import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 import '../styles/Layout.css'
-import { useState } from 'react'
 
 function App() {
-
-  const [cart, updateCart] = useState([])
-  
+	const [cart, updateCart] = useState([])
 	return (
 		<div>
 			<Banner>
@@ -17,8 +15,8 @@ function App() {
 				<h1 className='title'>La maison jungle</h1>
 			</Banner>
 			<div className='layout-inner'>
-				<Cart cart={cart} updateCart={updateCart}/>
-				<ShoppingList cart={cart} updateCart={updateCart}/>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
 			</div>
 			<Footer />
 		</div>
