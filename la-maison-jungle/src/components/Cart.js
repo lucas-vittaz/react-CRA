90e9e930cd5b4a4971ea106b4e7c09f445616bf0
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-08-04 08:48:18 +0200

    refactor Cart Component

--- la-maison-jungle/src/components/Cart.js
+++ la-maison-jungle/src/components/Cart.js
@@ -3,17 +3,17 @@ import '../styles/Cart.css'
 
 function Cart({ cart, updateCart }) {
 	const [isOpen, setIsOpen] = useState(true)
-	const total = cart.reduce(
-		(acc, plantType) => acc + plantType.amount * plantType.price,
+	const items = Object.keys(cart)
+	const total = items.reduce(
+		(acc, item) => acc + cart[item].amount * cart[item].price,
 		0
 	)
   useEffect(() =>{
     alert(`J'aurai ${total}€ à payer 💸`)
   }, [total])
-  
-  useEffect(() => {
-    document.title = `LMJ: ${total}€ d'achats`
-  }, [total])
+	useEffect(() => {
+		document.title = `LMJ: ${total}€ d'achats`
+	}, [total])
 
 	return isOpen ? (
 		<div className='cart'>
